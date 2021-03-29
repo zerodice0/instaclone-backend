85beb107880dc5966cde03aefcfc9990dc4ad0d8
Sung-jun Um <zerodice0@gmail.com>
2021-03-29 17:14:36 +0900

    3.11 clear.

---
+++ schema.js
@@ -0,0 +1,15 @@
+import {
+  loadFilesSync,
+  mergeTypeDefs,
+  makeExecutableSchema,
+  mergeResolvers
+} from "graphql-tools"
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`)
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`)
+const typeDefs = mergeTypeDefs(loadedTypes)
+const resolvers = mergeResolvers(loadedResolvers)
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+export default schema
--- server.js
+++ server.js
@@ -1,39 +1,13 @@
-import {ApolloServer, gql} from "apollo-server"
+import dotenv from 'dotenv'
+dotenv.config()
 
-const typeDefs = gql`
-  type Movie {
-    title: String
-    year: Int
-  },type Query {
-    movies: [Movie]
-    movie: Movie
-  },
-  type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
-  }
-`
+import {ApolloServer} from 'apollo-server'
+import schema from './schema'
 
-const resolvers = {
-  Query: {
-    movies: () => [],
-    movie: () => ({title: "Hello", "year": 2021}),
-  },
-  Mutation: {
-    createMovie: (_, {title}) => {
-      console.log(title)
-      return true
-    },
-    deleteMovie: (_, {title}) => {
-      console.log(title)
-      return true
-    }
-  }
-}
-
+const PORT = process.env.PORT
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 })
 
-server.listen().then(() => console.log("Server is running on http://localhost:4000/"))
+server.listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}/`))
