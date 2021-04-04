96aa6fcf1a4897d5f3baceb625551a93e9b0354d
zerodice0 <zerodice0@gmail.com>
2021-04-04 19:48:59 +0900

    - fix: 사용자 정보를 업데이트하는 editProfile resolver 추가. 	- 인자로 password값을 전달받았을 시, 해시값을 생성한 뒤 업데이트하도록 작성. 	- 조건부 구조 분해 할당을 사용하여, updatedUser.id값이 유효하지 않을 경우에만 error값이 return 객체에 포함되도록 작성.

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,6 +1,46 @@
 
+import bcrypt from "bcrypt"
+import client from "../../client"
+import { protectedResolver } from "../users.utils"
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi")
+    editProfile: protectedResolver(
+      async (
+        _,
+        {
+          firstName,
+          lastName,
+          username,
+          email,
+          password: newPassword,
+        },
+        {loggedInUser}
+      ) => {
+        let uglyPassword = null
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10)
+        }
+  
+        const updatedUser = await client.user.update(
+          {
+            where: {
+              id: loggedInUser?.id
+            },
+            data: {
+              firstName,
+              lastName,
+              username,
+              email,
+              ...(uglyPassword && {password: uglyPassword})
+            }
+          })
+  
+        return {
+          ok: updatedUser.id ? true : false,
+          ...(!updatedUser.id) && {error: "Could not update profile."}
+        }
+      }
+    )
   }
 }
