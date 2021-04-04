f30054b032607492ec6119a7e13aa44bf9a1a67c
zerodice0 <zerodice0@gmail.com>
2021-04-04 19:45:30 +0900

    - Question: 모야 sql파일은 깃헙에 올려도 되나?

--- prisma/migrations/20210404081046_/migration.sql
+++ prisma/migrations/20210404081046_/migration.sql
@@ -3,15 +3,17 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT,
-    "username" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User.username_unique" ON "User"("username");
+CREATE UNIQUE INDEX "User.userName_unique" ON "User"("userName");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
