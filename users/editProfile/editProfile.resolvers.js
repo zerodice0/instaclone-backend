a27d79dd35148bcdf48c1542ada25e038be650d6
zerodice0 <zerodice0@gmail.com>
2021-04-05 21:34:23 +0900

    - fix 	- editProfile에서 함수의 인자로 전달되는 일급함수 resolver를 inline으로 사용하지 않고 별도의 함수로 분리.

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -3,44 +3,44 @@ import bcrypt from "bcrypt"
 import client from "../../client"
 import { protectedResolver } from "../users.utils"
 
+const resolver = async (
+  _,
+  {
+    firstName,
+    lastName,
+    username,
+    email,
+    password: newPassword,
+  },
+  {loggedInUser}
+) => {
+  let uglyPassword = null
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10)
+  }
+
+  const updatedUser = await client.user.update(
+    {
+      where: {
+        id: loggedInUser?.id
+      },
+      data: {
+        firstName,
+        lastName,
+        username,
+        email,
+        ...(uglyPassword && {password: uglyPassword})
+      }
+    })
+
+  return {
+    ok: updatedUser.id ? true : false,
+    ...(!updatedUser.id) && {error: "Could not update profile."}
+  }
+}
+
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (
-        _,
-        {
-          firstName,
-          lastName,
-          username,
-          email,
-          password: newPassword,
-        },
-        {loggedInUser}
-      ) => {
-        let uglyPassword = null
-        if (newPassword) {
-          uglyPassword = await bcrypt.hash(newPassword, 10)
-        }
-  
-        const updatedUser = await client.user.update(
-          {
-            where: {
-              id: loggedInUser?.id
-            },
-            data: {
-              firstName,
-              lastName,
-              username,
-              email,
-              ...(uglyPassword && {password: uglyPassword})
-            }
-          })
-  
-        return {
-          ok: updatedUser.id ? true : false,
-          ...(!updatedUser.id) && {error: "Could not update profile."}
-        }
-      }
-    )
+    editProfile: protectedResolver(resolver)
   }
 }
