beaf79b7f7a6d6c7cd3a11c2df9f8a56d8159179
zerodice0 <zerodice0@gmail.com>
2021-04-12 23:21:08 +0900

    [add] 	- unfollow feature added.

---
+++ users/unfollowUser/unfollowUser.typeDefs.js
@@ -0,0 +1,12 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type UnfollowUserResult {
+    ok: Boolean!,
+    error: String
+  }
+
+  type Mutation {
+    unfollowUser(username: String!): UnfollowUserResult
+  }
+`
