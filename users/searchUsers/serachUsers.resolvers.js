1267973afae4d517d44c42235486d0b2fe508657
zerodice0 <zerodice0@gmail.com>
2021-04-19 22:14:13 +0900

    [add] - searchUsers type. - searchUsers resolvers.

---
+++ users/searchUsers/serachUsers.resolvers.js
@@ -0,0 +1,13 @@
+import client from "../../client"
+
+export default {
+  Query: {
+    searchUsers: async(_, {keyword}) => await client.user.findMany({where: {
+        username: {
+          startsWith: keyword.toLowerCase(),
+        }
+      }
+    })
+
+  }
+}
